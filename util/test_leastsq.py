f1f095bc2db5f6d021ad310f92b640411f512d13
68848jh <hui68848@163.com>
2022-04-07 22:45:28 +0800

    交替最小二乘法

--- util/test_leastsq.py
+++ util/test_leastsq.py
@@ -2,3 +2,19 @@
 # _*_coding=utf8_*_
 
 
+import numpy as np
+import matplotlib.pylab as plt
+
+data = np.loadtxt(
+    "D:\\pycharm\\test_rec\\data\\data.csv",
+    dtype=float,  # 数据类型float
+    usecols=(0,1),  # 要使用的列
+    encoding="utf8"  # 字符串编码格式
+)
+
+x = data[:, 0]  # 把第一列作为x轴数据
+y = data[:, 1]  # 把第二列作为y轴数据
+
+plt.scatter(x, y, color="red")
+
+plt.show()
